9bd0d0daa79b60183b835f3331066b263b68c7f9
Anatoly <algash04@gmail.com>
2022-09-27 11:27:03 +0300

    Refactored third lab

--- wwwroot/js/lab3.js
+++ wwwroot/js/lab3.js
@@ -24,8 +24,15 @@ connection.onreconnected(connectionId => {
 });
 
 function appendMessage(sender, message, color) {
-    document.querySelector('#messages-content').insertAdjacentHTML("beforeend", `<div style="color:${color}"><p>Sender: ${sender}</p><p>Action: ${message}</p></div><br>`);
+    document.getElementById('messages-content')
+        .insertAdjacentHTML(
+            "beforeend",
+            `<div style="color:${color}"><p>Sender: ${sender}</p><p>Action: ${message}</p></div><br>`);
 }
+
+const conState = document.getElementById('conState');
+const connectButton = document.getElementById('connectButton');
+const select = document.getElementById('role');
 
 async function connect() {
     if (connection.state === 'Disconnected') {
@@ -37,21 +44,23 @@ async function connect() {
             console.log(error);
         }
         if (connection.state === 'Connected') {
-            document.querySelector('#conState').textContent = 'Connected';
-            document.querySelector('#conState').style.color = 'green';
-            document.querySelector('#connectButton').textContent = 'Disconnect';
+            conState.textContent = 'Connected';
+            conState.style.color = 'green';
+            connectButton.textContent = 'Disconnect';
         }
     } else if (connection.state === 'Connected') {
         await connection.stop();
-        document.querySelector('#conState').textContent = 'Disconnected';
-        document.querySelector('#conState').style.color = 'red';
-        document.querySelector('#connectButton').textContent = 'Connect';
+
+        conState.textContent = 'Disconnected';
+        conState.style.color = 'red';
+        connectButton.textContent = 'Connect';
     }
 };
 
 async function sendMessage(toDo) {
     if (connection.state === 'Connected') {
         const message = { text: toDo };
+
         try {
             await connection.send('SendToOthers', message);
             appendMessage('Me', message.text, 'green');
@@ -59,14 +68,12 @@ async function sendMessage(toDo) {
         catch (error) {
             console.log(error);
         }
-        document.querySelector('#message').value = '';
     }
 };
 
 async function setMyName() {
     if (connection.state === 'Connected') {
         try {
-            const select = document.getElementById('role');
             await connection.send('SetMyName', select.value);
         }
         catch (error) {
